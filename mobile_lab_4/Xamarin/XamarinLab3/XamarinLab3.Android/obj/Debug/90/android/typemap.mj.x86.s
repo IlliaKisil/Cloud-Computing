d87e49da921ce74874193eb297999fd25db96df2
Illia Kisil <ikisil@edu.cdv.pl>
2021-06-23 17:40:59 +0300

    can't push this yesterday

---
+++ mobile_lab_4/Xamarin/XamarinLab3/XamarinLab3.Android/obj/Debug/90/android/typemap.mj.x86.s
@@ -0,0 +1,25 @@
+	/* Data SHA1: 0cef2680a008f5b03d54dc3c040b2a623e2a488e */
+	.file	"typemap.mj.inc"
+
+	/* Mapping header */
+	.section	.data.mj_typemap,"aw",@progbits
+	.type	mj_typemap_header, @object
+	.p2align	2
+	.global	mj_typemap_header
+mj_typemap_header:
+	/* version */
+	.long	1
+	/* entry-count */
+	.long	1434
+	/* entry-length */
+	.long	262
+	/* value-offset */
+	.long	145
+	.size	mj_typemap_header, 16
+
+	/* Mapping data */
+	.type	mj_typemap, @object
+	.global	mj_typemap
+mj_typemap:
+	.size	mj_typemap, 375709
+	.include	"typemap.mj.inc"
---
+++ mobile_lab_4/Xamarin/XamarinLab3/XamarinLab3/MainViewModel.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace XamarinLab3
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        string name = string.Empty;
+        public string Name
+        {
+            get => name;
+            set
+            {
+                if (name == value)
+                    return;
+                name = value;
+                OnPropertyChanged(nameof(Name));
+                OnPropertyChanged(nameof(DisplayName));
+            }
+        }
+
+        public string DisplayName => $"Name Entered: {Name}";
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        void OnPropertyChanged(string name)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+
+
+        string lastName = string.Empty;
+
+        public string LastName
+        {
+            get => lastName;
+
+            set
+            {
+                if (lastName == value)
+                    return;
+                lastName = value;
+                OnPropertyChangedLastName(nameof(lastName));
+                OnPropertyChangedLastName(nameof(DisplayLastName));
+            }
+
+        }
+
+        public string DisplayLastName => $"LastName Entered: {LastName}";
+
+        void OnPropertyChangedLastName(string lastName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(lastName));
+        }
+        
+
+        string phoneNumber = string.Empty;
+
+        public string PhoneNumber
+        {
+            get => phoneNumber;
+
+            set
+            {
+                if (phoneNumber == value)
+                    return;
+
+                phoneNumber = value;
+                OnPropertyChangedPhoneNumber(nameof(phoneNumber));
+                OnPropertyChangedPhoneNumber(nameof(DisplayPhoneNumber));
+            }
+
+        }
+
+        public string DisplayPhoneNumber => $"Phone Entered: {PhoneNumber}";
+        void OnPropertyChangedPhoneNumber(string phoneNumber)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(phoneNumber));
+        }
+    }
+}
